6138f7e6f61604268ba6937d851e9c086d7946ef
Priska Herger <priska.herger@birdsonmars.com>
2024-11-14 17:03:30 +0000

    A first decent but incomplete PAGE XML writer

--- kiebids/ocr_flow.py
+++ kiebids/ocr_flow.py
@@ -12,12 +12,15 @@ from kiebids import config, current_dataset, get_logger, pipeline_config
 from kiebids.modules.layout_analysis import LayoutAnalyzer
 from kiebids.modules.preprocessing import preprocessing
 from kiebids.modules.text_recognition import TextRecognizer
+from kiebids.modules.page_xml import write_page_xml
 
 pipeline_name = pipeline_config.pipeline_name
 logger = get_logger(pipeline_name)
 
 
 def ocr_flow():
+    os.makedirs(config.output_path, exist_ok=True)
+
     # init objects/models for every stage
     layout_analyzer = LayoutAnalyzer()
     text_recognizer = TextRecognizer()
@@ -38,19 +41,20 @@ def ocr_flow():
         )
 
         # accepts image and bounding boxes. returns. if debug the write snippets with corresponding text to disk
-        recognized_text = text_recognizer.run(  # noqa: F841
+        results = text_recognizer.run(  # noqa: F841
             image=preprocessed_image,
             bounding_boxes=[bb_label["bbox"] for bb_label in bb_labels],
             current_image_name=filename,
         )
 
         # semantic_labeling.run
-        # semantic_labeling_output_dir = semantic_labeling(layout_analysis_output_dir, output_path)
+        # semantic_labeling_output_dir = semantic_labeling(layout_analysis_output_dir, config.output_path)
 
         # entity_linking.run
-        # entity_linking(image_path, output_path)
+        # entity_linking(image_path, config.output_path)
 
-        # write results to PAGE Format
+        # write results to PAGE XML
+        write_page_xml(config.output_path, filename, results)
 
     # # Process images concurrently
     # futures = process_single_image.map(image_paths, OUTPUT_DIR)
