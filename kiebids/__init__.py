ade319abc4714f900baf29ba0f2942cb67214523
Eugen <eugen@birdsonmars.com>
2024-11-28 12:56:29 +0000

    choosing configs explicitly via env var

--- kiebids/__init__.py
+++ kiebids/__init__.py
@@ -8,18 +8,14 @@ from prefect.logging import get_logger
 
 load_dotenv()
 
-# if not set then default to prd
-environment = os.getenv("ENVIRONMENT", "prd")
+ocr_config = os.getenv("OCR_CONFIG", "ocr_config.yaml")
+workflow_config = os.getenv("WORKFLOW_CONFIG", "workflow_config.yaml")
 with open(
-    os.path.join(
-        os.path.dirname(__file__), f"../configs/workflow_{environment}_config.yaml"
-    )
+    os.path.join(os.path.dirname(__file__), f"../configs/{workflow_config}")
 ) as f:
     config = DotMap(yaml.safe_load(f))
 
-with open(
-    os.path.join(os.path.dirname(__file__), f"../configs/ocr_{environment}_config.yaml")
-) as f:
+with open(os.path.join(os.path.dirname(__file__), f"../configs/{ocr_config}")) as f:
     pipeline_config = DotMap(yaml.safe_load(f))
 
 current_dataset = None
