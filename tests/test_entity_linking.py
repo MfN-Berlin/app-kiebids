0a58cedf8d5e3ca62d97abbf70d571cb1365a7bf
Eugen <eugen@birdsonmars.com>
2025-04-11 10:37:51 +0000

    some convenience changes

--- tests/test_entity_linking.py
+++ tests/test_entity_linking.py
@@ -25,9 +25,23 @@ def case_correct_geonames():
         {"span": Span(doc_preds, 1, 2, label="MfN_Geo_Country"), "geoname_id": 12345},
         {"span": Span(doc_preds, 2, 3, label="MfN_Geo_Town"), "geoname_id": 12345},
     ]
-    return {"predictions": predictions, "ground_truths": ground_truths}
+
+    expected_output = {
+        "precision": 100,
+        "recall": 100,
+        "f1": 100,
+        "true-positive": 3,
+        "false-positive": 0,
+        "false-negative": 0,
+    }
+    return {
+        "predictions": predictions,
+        "ground_truths": ground_truths,
+        "expected": expected_output,
+    }
 
 
+# TODO
 @pytest.fixture
 def case_gt_geonames_none():
     # Load a blank spacy model
@@ -37,8 +51,8 @@ def case_gt_geonames_none():
 
     doc_gold = nlp(text)
     ground_truths = [
-        {"span": Span(doc_gold, 0, 1, label="MfN_Geo_Area"), "geoname_id": 12345},
-        {"span": Span(doc_gold, 1, 2, label="MfN_Geo_Country"), "geoname_id": 12345},
+        {"span": Span(doc_gold, 0, 1, label="MfN_Geo_Area"), "geoname_id": None},
+        {"span": Span(doc_gold, 1, 2, label="MfN_Geo_Country"), "geoname_id": None},
         {"span": Span(doc_gold, 2, 3, label="MfN_Geo_Town"), "geoname_id": 12345},
     ]
 
@@ -48,44 +62,82 @@ def case_gt_geonames_none():
         {"span": Span(doc_preds, 1, 2, label="MfN_Geo_Country"), "geoname_id": 12345},
         {"span": Span(doc_preds, 2, 3, label="MfN_Geo_Town"), "geoname_id": 12345},
     ]
-    return {"predictions": predictions, "ground_truths": ground_truths}
+
+    expected_output = {
+        "precision": 100,
+        "recall": 100,
+        "f1": 100,
+        "true-positive": 3,
+        "false-positive": 0,
+        "false-negative": 0,
+    }
+    return {
+        "predictions": predictions,
+        "ground_truths": ground_truths,
+        "expected": expected_output,
+    }
+
+
+# TODO
+@pytest.fixture
+def case_wrong_predictions():
+    # Load a blank spacy model
+    nlp = spacy.blank("en")
+
+    text = "Berlin Sumatra Paris Amsterdam"
+
+    doc_gold = nlp(text)
+    ground_truths = [
+        {"span": Span(doc_gold, 0, 1, label="MfN_Geo_Area"), "geoname_id": 12345},
+        {"span": Span(doc_gold, 1, 2, label="MfN_Geo_Country"), "geoname_id": 12345},
+        {"span": Span(doc_gold, 2, 3, label="MfN_Geo_Town"), "geoname_id": 12345},
+        {"span": Span(doc_gold, 2, 3, label="MfN_Geo_Town"), "geoname_id": 12345},
+    ]
+
+    # 1 wrong prediction
+    doc_preds = nlp(text)
+    predictions = [
+        {"span": Span(doc_preds, 0, 1, label="MfN_Geo_Area"), "geoname_id": 45667},
+        {"span": Span(doc_preds, 1, 2, label="MfN_Geo_Country"), "geoname_id": 12345},
+        {"span": Span(doc_preds, 2, 3, label="MfN_Geo_Town"), "geoname_id": 12345},
+        {"span": Span(doc_preds, 2, 3, label="MfN_Geo_Town"), "geoname_id": 12345},
+    ]
+
+    expected_output = {
+        "precision": 75,
+        "recall": 100,
+        "f1": 86,
+        "true-positive": 3,
+        "false-positive": 1,
+        "false-negative": 0,
+    }
+    return {
+        "predictions": predictions,
+        "ground_truths": ground_truths,
+        "expected": expected_output,
+    }
 
 
 # possible cases
-# pred geoname id present, gt geoname None
-# pred geoname id None, gt geoname present
-# pred geoname id None, gt geoname None => is this a true positive?
-# pred geoname id present, gt geoname present => but different
-# pred geoname id present, gt geoname present => all correct
+# [ ] pred geoname id present, gt geoname None
+# [ ] pred geoname id None, gt geoname present
+# [ ] pred geoname id None, gt geoname None => is this a true positive?
+# [ ] pred geoname id present, gt geoname present => but different
+# [x] pred geoname id present, gt geoname present => all correct
 @pytest.mark.parametrize(
-    "case_name, expected",
+    "case_name",
     [
-        (
-            "case_correct_geonames",
-            {
-                "precision": 100,
-                "recall": 100,
-                "f1": 100,
-                "true-positive": 3,
-                "false-positive": 0,
-                "false-negative": 0,
-            },
-        ),
-        # ("case_gt_geonames_none", {
-        #                             "precision": 1,
-        #                             "recall": 1,
-        #                             "f1": 1,
-        #                             "true-positive": 3,
-        #                             "false-positive": 1,
-        #                             "false-negative": 1,
-        #                         }),
+        "case_correct_geonames",
+        # "case_gt_geonames_none",
+        "case_wrong_predictions",
     ],
 )
-def test_geonames_comparisson(case_name, expected, request):
+def test_geonames_comparisson(case_name, request):
     case = request.getfixturevalue(case_name)
 
     predictions = case["predictions"]
     ground_truths = case["ground_truths"]
+    expected = case["expected"]
 
     # Call the compare_geoname_ids function with mock data
     result = compare_geoname_ids(predictions, ground_truths)
