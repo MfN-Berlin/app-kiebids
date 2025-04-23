5cd09c0c3a2fd3be3940472590ed54465eec313d
birdLova <lova.wahlin@birdsonmars.com>
2025-04-23 12:06:28 +0000

    update tests

--- tests/test_semantic_tagging.py
+++ tests/test_semantic_tagging.py
@@ -2,132 +2,107 @@ import pytest
 
 from kiebids.modules.semantic_tagging import SpacyMatcher
 
+# class SemanticTagging:
+#     def __init__(self):
+#         self.model_regex = SpacyMatcher()
 
-class SemanticTagging:
-    def __init__(self):
-        self.model_regex = SpacyMatcher()
-
-    def run(self, texts):  # pylint: disable=unused-argument
-        st_result = []
-        for text in texts:
-            output = self.model_regex.get_regex_tags(text)
-            st_result.append(output)
-        return st_result
+#     def run(self, texts):  # pylint: disable=unused-argument
+#         st_result = []
+#         for text in texts:
+#             output = self.model_regex.get_regex_tags(text)
+#             st_result.append(output)
+#         return st_result
 
 
 @pytest.fixture
-def semantic_tagging_instance():
-    return SemanticTagging()
+def spacy_matcher_instance():
+    return SpacyMatcher()
 
 
-def test_semantic_tagging(semantic_tagging_instance):
+def test_semantic_tagging(spacy_matcher_instance):
     """
-    Test the `run` method of the `semantic_tagging_instance` for various types of text inputs
+    Test the SpacyMatcher for various types of text inputs
     and their expected semantic tagging outputs.
     Args:
-        semantic_tagging_instance: An instance of a semantic tagging class with a `run` method
+        spacy_matcher_instance: An instance of the SpacyMatcher class with a `run` method
                                    that processes text and returns tagged entities.
     Asserts:
         Verifies that the output of the `run` method matches the expected output for each test case.
     """
 
-    text_gatheringdate = [
-        "Sample text with different dates: 12.03.2023",
-        "and 4.II.1555",
-        "and also VI.93",
-    ]
+    text_gatheringdate = (
+        "Sample text with different dates: 12.03.2023, and 4.II.1555 and also VI.93"
+    )
 
     expected_gatheringdate = [
-        [("MfN_GatheringDate", 34, 10)],
-        [("MfN_GatheringDate", 4, 9)],
-        [("MfN_GatheringDate", 9, 5)],
+        ("MfN_GatheringDate", 34, 10),
+        ("MfN_GatheringDate", 50, 9),
+        ("MfN_GatheringDate", 69, 5),
     ]
 
-    text_geolongitude = ["Sample text with coordinates: 13°23'E and 52°30'W"]
-    expected_geolongitude = [
-        [("MfN_Geo_Longitude", 30, 7), ("MfN_Geo_Longitude", 42, 7)]
-    ]
+    text_geolongitude = "Sample text with coordinates: 13°23'E and 52°30'W"
+    expected_geolongitude = [("MfN_Geo_Longitude", 30, 7), ("MfN_Geo_Longitude", 42, 7)]
 
-    text_geolatitude = ["Sample text with coordinates: 13°23'N and 52°30'S"]
-    expected_geolatitude = [[("MfN_Geo_Latitude", 30, 7), ("MfN_Geo_Latitude", 42, 7)]]
+    text_geolatitude = "Sample text with coordinates: 13°23'N and 52°30'S"
+    expected_geolatitude = [("MfN_Geo_Latitude", 30, 7), ("MfN_Geo_Latitude", 42, 7)]
 
-    text_nuri = ["Sample text with a URL: http://coll.mfn-berlin.de/u/abc123"]
-    expected_nuri = [[("MfN_NURI", 24, 34)]]
+    text_nuri = "Sample text with a URL: http://coll.mfn-berlin.de/u/abc123"
+    expected_nuri = [("MfN_NURI", 24, 34)]
 
-    text_sex = ["text with symbols: ♂ and ♀"]
-    expected_sex = [[("MfN_Sex", 19, 1), ("MfN_Sex", 25, 1)]]
+    text_sex = "text with symbols: ♂ and ♀"
+    expected_sex = [("MfN_Sex", 19, 1), ("MfN_Sex", 25, 1)]
 
-    text_type = [
+    text_type = (
         "text with type: holotype and lectotype and neotyp and paralectotype and syntyp"
-    ]
+    )
 
     expected_type = [
-        [
-            ("MfN_Type", 10, 4),
-            ("MfN_Type", 16, 8),
-            ("MfN_Type", 29, 9),
-            ("MfN_Type", 43, 6),
-            ("MfN_Type", 54, 13),
-            ("MfN_Type", 72, 6),
-        ]
+        ("MfN_Type", 10, 4),
+        ("MfN_Type", 16, 8),
+        ("MfN_Type", 29, 9),
+        ("MfN_Type", 43, 6),
+        ("MfN_Type", 54, 13),
+        ("MfN_Type", 72, 6),
     ]
 
     result = [
-        [
-            (span.label_, span.start_char, span.end_char - span.start_char)
-            for span in spans
-        ]
-        for spans in semantic_tagging_instance.run(text_gatheringdate)
+        (span.label_, span.start_char, span.end_char - span.start_char)
+        for span in spacy_matcher_instance.get_regex_tags(text_gatheringdate)
     ]
 
     assert result == expected_gatheringdate
 
     result = [
-        [
-            (span.label_, span.start_char, span.end_char - span.start_char)
-            for span in spans
-        ]
-        for spans in semantic_tagging_instance.run(text_geolongitude)
+        (span.label_, span.start_char, span.end_char - span.start_char)
+        for span in spacy_matcher_instance.get_regex_tags(text_geolongitude)
     ]
 
     assert result == expected_geolongitude
 
     result = [
-        [
-            (span.label_, span.start_char, span.end_char - span.start_char)
-            for span in spans
-        ]
-        for spans in semantic_tagging_instance.run(text_geolatitude)
+        (span.label_, span.start_char, span.end_char - span.start_char)
+        for span in spacy_matcher_instance.get_regex_tags(text_geolatitude)
     ]
 
     assert result == expected_geolatitude
 
     result = [
-        [
-            (span.label_, span.start_char, span.end_char - span.start_char)
-            for span in spans
-        ]
-        for spans in semantic_tagging_instance.run(text_nuri)
+        (span.label_, span.start_char, span.end_char - span.start_char)
+        for span in spacy_matcher_instance.get_regex_tags(text_nuri)
     ]
 
     assert result == expected_nuri
 
     result = [
-        [
-            (span.label_, span.start_char, span.end_char - span.start_char)
-            for span in spans
-        ]
-        for spans in semantic_tagging_instance.run(text_sex)
+        (span.label_, span.start_char, span.end_char - span.start_char)
+        for span in spacy_matcher_instance.get_regex_tags(text_sex)
     ]
 
     assert result == expected_sex
 
     result = [
-        [
-            (span.label_, span.start_char, span.end_char - span.start_char)
-            for span in spans
-        ]
-        for spans in semantic_tagging_instance.run(text_type)
+        (span.label_, span.start_char, span.end_char - span.start_char)
+        for span in spacy_matcher_instance.get_regex_tags(text_type)
     ]
 
     assert result == expected_type
